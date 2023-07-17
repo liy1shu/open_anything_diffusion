8e964de71f267d7fa4b7e61e0216887f168da64a
liy1shu <liyishu19@mails.tsinghua.edu.cn>
2023-07-17 16:57:44 -0400

    data identical check and test frame

--- tests/single_trajectory_test.py
+++ tests/single_trajectory_test.py
@@ -1,1 +1,8 @@
-# Check single trajectory should produce the same training results as original flowbot
+# Check len=1 trajectory should produce the same training results as original flowbot
+def check_single_trajectory():
+    # Run 10 epoch of original flowbot model
+
+    # Run 10 epoch of 1-trajectory model
+
+    # Compare the weights: r they the same (diff < eps)
+    return
